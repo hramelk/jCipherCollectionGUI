513b2abc3b2fc9ffb08af0bfeccb15bb063fb530
hramelk <27568117+hramelk@users.noreply.github.com>
2018-02-12 22:06:01 +0400

    Removed unneeded lines from main()

--- CipherCollectionGUI/src/ccgui/CipherCollection.java
+++ CipherCollectionGUI/src/ccgui/CipherCollection.java
@@ -4,11 +4,6 @@ public class CipherCollection {
 	
 	public static void main(String[] args) {
 		new GUI();
-		System.out.println(Atbash.encode("blabla"));
-		System.out.println(Atbash.encode("yozyoz"));
-		
-		System.out.println(Atbash.encode("BlaBla"));
-		System.out.println(Atbash.encode("YozYoz"));
 	}
 	
 }
